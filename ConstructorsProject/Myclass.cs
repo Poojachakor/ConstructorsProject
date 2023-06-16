fe7d57457f097b63b4cd7a2726c657d8b97fb9ad
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 21:07:05 +0530

    Add: UC3 file

---
+++ ConstructorsProject/Myclass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsProject
+{
+     public class Myclass
+     {
+        public int publicField;            // Accessible from anywhere
+        private string privateField;       // Accessible only within this class
+        protected bool protectedField;     // Accessible within this class and derived classes
+        internal double internalField;     // Accessible within the same assembly
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public method");
+        }
+
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private method");
+        }
+
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine("Protected method");
+        }
+
+        internal void InternalMethod()
+        {
+            Console.WriteLine("Internal method");
+        }
+    }
+
+    public class DerivedClass : Myclass
+    {
+        public void AccessProtectedField()
+        {
+            protectedField = true;           
+        }
+
+        public void AccessProtectedMethod()
+        {
+            ProtectedMethod();               // Accessing protected method from derived class
+        }
+    }
+}
+
--- ConstructorsProject/Program.cs
+++ ConstructorsProject/Program.cs
@@ -6,13 +6,24 @@ namespace ConstructorsProject
     {
         static void Main(string[] args)
         {
-            IDrawable drawable;
-            drawable = new Addition();
-            drawable.Operation();
-            drawable = new Subtraction();
-            drawable.Operation();
-            drawable = new Multiplication();
-            drawable.Operation();
+
+            Myclass myObj = new Myclass();
+            myObj.publicField = 10;           // Accessing public field
+            myObj.PublicMethod();             // Accessing public method
+
+            // The following lines will cause compilation errors
+            // myObj.privateField = "private";     // Cannot access private field outside the class
+            // myObj.PrivateMethod();              // Cannot access private method outside the class
+
+            DerivedClass derivedObj = new DerivedClass();
+            derivedObj.AccessProtectedField();    // Accessing protected field from derived class
+            derivedObj.AccessProtectedMethod();   // Accessing protected method from derived class
+
+            // The following lines will cause compilation errors
+            // derivedObj.protectedField = true;     // Cannot access protected field outside the class hierarchy
+            // derivedObj.ProtectedMethod();         // Cannot access protected method outside the class hierarchy
         }
     }
+
 }
+
