3d38efd776007fb8262b0f11bf08b4e4a2c5fbed
Poojachakor <chakorpooja07@gmail.com>
2023-06-16 20:50:25 +0530

    Add: ConstructorsProject

---
+++ ConstructorsProject/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsProject
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        // Default constructor
+        public Car()
+        {
+            Make = "Unknown";
+            Model = "Unknown";
+            Year = 0;
+        }
+
+        // Parameterized constructor
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        // Copy constructor
+        public Car(Car otherCar)
+        {
+            Make = otherCar.Make;
+            Model = otherCar.Model;
+            Year = otherCar.Year;
+        }
+
+        // Static constructor
+        static Car()
+        {
+            Console.WriteLine("Static constructor called.");
+        }
+
+        // Instance method
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}");
+        }
+    }
+}
+
+    
---
+++ ConstructorsProject/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConstructorsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using the default constructor
+            Car defaultCar = new Car();
+            defaultCar.DisplayCarInfo();  
+
+            // Using the parameterized constructor
+            Car hondaCivic = new Car("Honda", "Civic", 2022);
+            hondaCivic.DisplayCarInfo();  
+
+            // Using the copy constructor
+            Car copiedCar = new Car(hondaCivic);
+            copiedCar.DisplayCarInfo();
+        }
+    }
+}
